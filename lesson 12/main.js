cf1b8b8713eee74a3a839d53398a176a4c2e5290
Deti8008 <xh.vejs@gmail.com>
2025-10-09 20:33:55 +0200

    b

---
+++ lesson 12/main.js
@@ -0,0 +1,21 @@
+$('#sort').sortable();
+
+// $('#accordion').accordion();
+
+$(function(){
+    $('#accordion').accordion({
+    collapsible: true
+});
+});
+
+
+$(function(){
+    $('#tabs').tabs();
+});
+
+
+$(function(){
+    $('#tabs').accordion({
+    collapsible: true
+});
+});
